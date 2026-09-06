372d17dbdbee8bd3fc756a81e536f580059ddeca
wdmzsyy <2037525426@qq.com>
2026-09-06 21:12:03 +0800

    实现组件play环境

---
+++ play/src/env.d.ts
@@ -0,0 +1,9 @@
+// 让 TypeScript 不再对 .vue 文件报错，并且能识别 Vite 的特殊 API
+/// <reference types="vite/client" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
---
+++ play/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// 默认渲染组件进行挂载
+createApp(App).mount('#app')
